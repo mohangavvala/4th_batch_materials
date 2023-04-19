de7ea5f1440dcdea5a1262216933d2781872d474
Jagangavvala Mohan <jaganmohan.gavvala@gmail.com>
2023-04-19 07:23:25 +0530

    first commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+a=100
+b=200
+
+def total(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+def upper_case(x):
+    return x.upper()
+ 
---
+++ test (2).py
@@ -0,0 +1,11 @@
+a=10
+print(a)
+print(id(a))
+b=10.5
+print(b)
+print(id(b))
+
+c="HELLO"
+print(c)
+
+print(id(c))
